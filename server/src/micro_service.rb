a9c827976e3e2b4d07a41e997b34758fbd1304eb
Jon Jagger <jon@jaggersoft.com>
2016-10-10 17:43:30 +0100

    refactoring

--- server/src/micro_service.rb
+++ server/src/micro_service.rb
@@ -13,25 +13,21 @@ class MicroService < Sinatra::Base
 
   get '/pulled' do
     content_type :json
-    request.body.rewind
     runner.pulled?(image_name)
   end
 
   post '/pull' do
     content_type :json
-    request.body.rewind
     runner.pull(image_name)
   end
 
   post '/start' do
     content_type :json
-    request.body.rewind
     runner.start(kata_id, avatar_name)
   end
 
   post '/run' do
     content_type :json
-    request.body.rewind
     max_seconds = args['max_seconds']
     delete_filenames = args['delete_filenames']
     changed_files = args['changed_files']
@@ -49,7 +45,12 @@ class MicroService < Sinatra::Base
   end
 
   def args
-    @args ||= JSON.parse(request.body.read)
+    @args ||= request_body_args
+  end
+
+  def request_body_args
+    request.body.rewind
+    JSON.parse(request.body.read)
   end
 
   def image_name
