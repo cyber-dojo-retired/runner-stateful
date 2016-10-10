2f444335f57cdbc04fb76010ff354c6931650276
Jon Jagger <jon@jaggersoft.com>
2016-10-10 22:47:17 +0100

    fix demo.sh

--- demo.sh
+++ demo.sh
@@ -2,9 +2,17 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-cd ${my_dir} && ./build.sh
-ip=$(docker-machine ip default)
-echo "${ip}:4558/diff"
+app_dir=/app
+docker_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
+client_port=4558
+server_port=4557
 
+${my_dir}/build.sh ${app_dir} ${docker_version} ${client_port} ${server_port}
+ip=$(docker-machine ip default)
+echo "${ip}:${client_port}"
+
+export DOCKER_ENGINE_VERSION=${docker_version}
+export CLIENT_PORT=${client_port}
+export SERVER_PORT=${server_port}
 docker-compose down
 docker-compose up &
