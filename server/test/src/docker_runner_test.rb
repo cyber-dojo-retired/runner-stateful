c2c963a18185f9318d6d5edab64ece837733efe1
Jon Jagger <jon@jaggersoft.com>
2016-10-13 10:56:07 +0100

    added first test for runner.pulled?

---
+++ server/test/src/docker_runner_test.rb
@@ -0,0 +1,56 @@
+
+require_relative './lib_test_base'
+require_relative './mock_sheller'
+
+class DockerRunnerTest < LibTestBase
+
+  def self.hex(suffix)
+    '9D9' + suffix
+  end
+
+  def setup
+    super
+    ENV[env_name('shell')]  = 'MockSheller'
+  end
+
+  def teardown
+    shell.teardown
+    super
+  end
+
+  test 'B71',
+  'pulled?(image_name) is false when image_name has not yet been pulled' do
+    image_name = 'cyberdojofoundation/gcc_assert'
+
+    command = [ sudo, 'docker', 'images' ].join(space)
+    output = [
+      "REPOSITORY                                 TAG                 IMAGE ID            CREATED             SIZE",
+      "cyberdojofoundation/java_cucumber          latest              06aa46aad63d        6 weeks ago         881.7 MB",
+      "cyberdojo/runner                           1.12.1              a531a83580c9        18 minutes ago      56.05 MB"
+    ].join("\n")
+    shell.mock_exec([command], output, success)
+
+    refute runner.pulled?(image_name)
+  end
+
+  private
+
+  include Externals
+
+  def runner
+    DockerRunner.new(self)
+  end
+
+  def success
+    0
+  end
+
+  def sudo
+    'sudo -u docker-runner sudo'
+  end
+
+  def space
+    ' '
+  end
+
+end
