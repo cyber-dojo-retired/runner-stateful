58e31ca3cf08ff88cfeac2ec3a5d2f90f5c77bd9
Jon Jagger <jon@jaggersoft.com>
2016-10-13 10:58:59 +0100

    added runner.pulled?(image_name) true test

--- server/test/src/docker_runner_test.rb
+++ server/test/src/docker_runner_test.rb
@@ -21,7 +21,6 @@ class DockerRunnerTest < LibTestBase
   test 'B71',
   'pulled?(image_name) is false when image_name has not yet been pulled' do
     image_name = 'cyberdojofoundation/gcc_assert'
-
     command = [ sudo, 'docker', 'images' ].join(space)
     output = [
       "REPOSITORY                                 TAG                 IMAGE ID            CREATED             SIZE",
@@ -29,9 +28,26 @@ class DockerRunnerTest < LibTestBase
       "cyberdojo/runner                           1.12.1              a531a83580c9        18 minutes ago      56.05 MB"
     ].join("\n")
     shell.mock_exec([command], output, success)
-
     refute runner.pulled?(image_name)
   end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '94E',
+  'pulled?(image) is true when image_name has already been pulled' do
+    image_name = 'cyberdojofoundation/gcc_assert'
+    command = [ sudo, 'docker', 'images' ].join(space)
+    output = [
+      "REPOSITORY                                 TAG                 IMAGE ID            CREATED             SIZE",
+      "cyberdojofoundation/java_cucumber          latest              06aa46aad63d        6 weeks ago         881.7 MB",
+      "cyberdojo/runner                           1.12.1              a531a83580c9        18 minutes ago      56.05 MB",
+      "cyberdojofoundation/gcc_assert             latest              da213d286ec5        4 months ago        99.16 MB"
+    ].join("\n")
+    shell.mock_exec([command], output, success)
+    assert runner.pulled?(image_name)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   private
 
