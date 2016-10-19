e5cc867a3963efc0f3fc475ef08c24b4524cb24e
Jon Jagger <jon@jaggersoft.com>
2016-10-19 12:47:42 +0100

    delete stdout/stderr dumping and add comments on next step

--- server/test/src/sudo_docker_test.rb
+++ server/test/src/sudo_docker_test.rb
@@ -30,14 +30,7 @@ class SudoDockerTest < LibTestBase
     # o) on a proper Travis CI Linux box it can't be run, and this test passes
     command = "docker images >#{stdoutFile} 2>#{stderrFile}"
     output, exit_status = shell.exec([command])
-    p "B4C stdout---------"
-    p `whoami`
-    p `cat #{stdoutFile}`
-    p "B4C stderr---------"
-    p `cat #{stderrFile}`
-    p "---------"
-
-    refute_equal success, exit_status, '[docker image] can be run without sudo'
+    refute_equal success, exit_status, '[docker image] can be run without sudo!!'
     assert `cat #{stderrFile}`.start_with? 'Cannot connect to the Docker daemon'
   end
 
@@ -47,55 +40,58 @@ class SudoDockerTest < LibTestBase
   'sudo docker command succeeds and exits zero' do
     # NB: sudo [docker images]...
     # o) locally on a Mac using Docker-Toolbox this test is no good (see above)
-    # o) on a proper Travis CI Linux box this test is currently failing...
+    # o) on a proper Travis CI Linux box this test is currently passing...
     command = "#{sudo} docker images >#{stdoutFile} 2>#{stderrFile}"
     output, exit_status = shell.exec([command])
-    p "279 stdout---------"
-    p `whoami`
-    p `cat #{stdoutFile}`
-    p "279 stderr---------"
-    p `cat #{stderrFile}`
-    p "---------"
     assert_equal success, exit_status
     docker_images = `cat #{stdoutFile}`
     assert docker_images.include? 'cyberdojo/runner'
   end
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # On Ubuntu 14.04 host, after installing docker,
-  # if you [$ cat /etc/group] you see
-  #    docker:x:999:
-  # Do all installs install docker with the same group-id?
-  #
-  # if you [$ cd /var/run && ls -al] you see
-  #    srw-rw----  1 root     docker        0 Oct 19 10:17 docker.sock
-  #
-  # Inside the runner:1.12.2 container
-  # if you [$ cat /etc/group] you see
-  #    ping:x:999:
-  # if you [$ cd /var/run && ls -al] you see
-  #    srw-rw----  1 root     ping          0 Oct 19 10:17 docker.sock
-  #
-  # So owner:rw-
-  #    group:rw-
-  #    everyone:---
-  #
-  # I group membership making it work?
-  # How can I tell if cyber-dojo user a member of ping?
-  # $ groups
-  # Nope.
-  # Not groups. I think /var/run/docker.sock is not the issue.
-  #
-  #
-  # On Ubuntu 14.04 host, after installing docker,
-  # if you [$ cd /usr/bin && ls -al docker*] you see
-  # -rwxr-xr-x 1 root root 15096408 Oct 11 18:19 docker
-  #
-  # Inside the runner:1.12.2 container
-  # $ if you [$ cd /usr/bin && ls -al docker*]
-  # -rwxr-xr-x 1 root root 15673856 Oct 11 17:05 docker
-  #
-  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Why do the above tests break on Docker-Toolbox setup?
+    # Get them working on both?
+    # Detect if on Docker-Toolbox and don't run?
+    # - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # On Ubuntu 14.04 host, after installing docker,
+    # if you [$ cat /etc/group] you see
+    #    docker:x:999:
+    # if you [$ cd /var/run && ls -al] you see
+    #    srw-rw----  1 root     docker        0 Oct 19 10:17 docker.sock
+    #
+    # On Mac with Docker-Toolbox
+    # $ docker-machine ssh default
+    # $ whoami
+    # $ docker run --rm -i --tty --volume=/var/run/docker.sock:/var/run/docker.sock \
+    #     cyberdojo/runner:1.12.2 sh
+    # $ cd /var/run
+    # $ ls -al
+    # srw-rw----    1 root     users            0 Oct 14 19:33 docker.sock
+    # $ cat /etc/group
+    # users:x:100:games
+    #
+    #
+    #
+    # Inside the runner:1.12.2 container on Mac with Docker-Toolbox
+    # if you [$ cat /etc/group] you see
+    #    ping:x:999:
+    # if you [$ cd /var/run && ls -al] you see
+    #    srw-rw----  1 root     ping          0 Oct 19 10:17 docker.sock
+    #
+    # So owner:rw-
+    #    group:rw-
+    #    everyone:---
+    #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # On Ubuntu 14.04 host, after installing docker,
+    # if you [$ cd /usr/bin && ls -al docker*] you see
+    # -rwxr-xr-x 1 root root 15096408 Oct 11 18:19 docker
+    #
+    # Inside the runner:1.12.2 container
+    # $ if you [$ cd /usr/bin && ls -al docker*]
+    # -rwxr-xr-x 1 root root 15673856 Oct 11 17:05 docker
+    #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   private
 
