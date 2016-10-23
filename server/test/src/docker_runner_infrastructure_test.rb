1cee6dd56969ab73407f936980976392250c6271
Jon Jagger <jon@jaggersoft.com>
2016-10-23 21:13:49 +0100

    added test that a changed file has its size and time-stamp updated

--- server/test/src/docker_runner_infrastructure_test.rb
+++ server/test/src/docker_runner_infrastructure_test.rb
@@ -21,7 +21,7 @@ class DockerRunnerInfrastructureTest < LibTestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '385',
-  'deleted files get deleted and all previous files still exist' do
+  'deleted files are removed and all previous files still exist' do
     runner_start
     files = language_files('gcc_assert')
     files['cyber-dojo.sh'] = 'ls'
@@ -36,7 +36,7 @@ class DockerRunnerInfrastructureTest < LibTestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '232',
-  'new files get added and all previous files still exist' do
+  'new files are added and all previous files still exist' do
     runner_start
     files = language_files('gcc_assert')
     files['cyber-dojo.sh'] = 'ls'
@@ -52,7 +52,7 @@ class DockerRunnerInfrastructureTest < LibTestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '4E8',
-  'unchanged files dont get resaved' do
+  "unchanged files still exist and don't get touched at all" do
     runner_start
     files = language_files('gcc_assert')
     files['cyber-dojo.sh'] = 'ls -el'
@@ -63,25 +63,63 @@ class DockerRunnerInfrastructureTest < LibTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # test '9A7'
-  # 'changed files get resaved' do
-  # lines like this
-  # -rwxr-xr-x 1 nobody root 19 Sun Oct 23 19:15:35 2016 cyber-dojo.sh
-  # 0          1 2      3    4  5   6   7  8        9    10
-  #before = ls_output.split("\n")
-  #info = lines[0].split
-  #p "permissions:#{info[0]}"
-  #p "???#{info[1]}"
-  #p "owner:#{info[2]}"
-  #p "group:#{info[3]}"
-  #p "name:#{info[10]}"
-  # end
+  test '9A7',
+  'a changed file is resaved and its size and time-stamp updates' do
+    runner_start
+    files = language_files('gcc_assert')
+    files['cyber-dojo.sh'] = 'ls -el | tail -n +2'
+    ls_output = runner_run(files)
+    # each line looks like this...
+    # -rwxr-xr-x 1 nobody root 19 Sun Oct 23 19:15:35 2016 cyber-dojo.sh
+    before = parse(ls_output)
+    assert_equal 5, before.size
+    sleep 2
+    hiker_h = files['hiker.h']
+    extra = '//hello'
+    files = { 'hiker.h' => hiker_h + extra }
+    ls_output = runner_run(files)
+    after = parse(ls_output)
+
+    assert_equal before.keys, after.keys
+    before.keys.each do |filename|
+      was = before[filename]
+      now = after[filename]
+      same = lambda { |name| assert_equal was[name], now[name] }
+      same.call('permissions')
+      same.call('user')
+      same.call('group')
+      if filename == 'hiker.h'
+        refute_equal now['time-stamp'], was['time-stamp']
+        assert_equal now['size'], was['size'] + extra.size
+      else
+        same.call('time-stamp')
+        same.call('size')
+      end
+    end
+  end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   private
 
   include DockerRunnerHelpers
+
+  def parse(ls_output)
+    # each line looks like this...
+    # -rwxr-xr-x 1 nobody root 19 Sun Oct 23 19:15:35 2016 cyber-dojo.sh
+    # 0          1 2      3    4  5   6   7  8        9    10
+    Hash[ls_output.split("\n").collect { |line|
+      info = line.split
+      filename = info[10]
+      [filename, {
+        'permissions' => info[0],
+        'user'        => info[2],
+        'group'       => info[3],
+        'size'        => info[4].to_i,
+        'time-stamp'  => info[8],
+      }]
+    }]
+  end
 
 end
 
