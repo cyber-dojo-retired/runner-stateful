562bf1dc9e8090d53ac54ffe6262d10347c63593
Jon Jagger <jon@jaggersoft.com>
2016-10-25 21:51:48 +0100

    refactoring

--- shell_in.sh
+++ shell_in.sh
@@ -2,11 +2,13 @@
 
 user=${1:-cyber-dojo}
 
+docker_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
+
 docker run \
   --rm \
   --user=${user} \
   --interactive \
   --tty \
   --volume=/var/run/docker.sock:/var/run/docker.sock \
-  cyberdojo/runner:1.12.2 \
+  cyberdojo/runner:${docker_version} \
   sh
