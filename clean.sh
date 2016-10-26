80499b2f1ea0486e6bcb2c40b7e374f2a0861120
Jon Jagger <jon@jaggersoft.com>
2016-10-26 12:43:15 +0100

    [ci skip] remove clean.sh

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# remove images
-docker images --quiet --filter dangling=true | xargs docker rmi --force
-# remove containers
-docker ps     --quiet --filter status=exited | xargs docker rm --force
