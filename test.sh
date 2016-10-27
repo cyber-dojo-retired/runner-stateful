909ef5a9c89b99a4eb7378ea3e7032c611625e1e
Jon Jagger <jon@jaggersoft.com>
2016-10-27 10:19:06 +0100

    refactoring; test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
-export DOCKER_ENGINE_VERSION=${docker_engine_version}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 # server
@@ -13,33 +11,37 @@ docker cp ${server_cid}:/tmp/coverage ${my_dir}/server
 echo "Coverage report copied to ${my_dir}/server/coverage"
 cat ${my_dir}/server/coverage/done.txt
 
+if [ ${server_status} != 0 ]; then
+  echo
+  echo "server: cid = ${server_cid}, status = ${server_status}"
+  echo
+  exit 1
+fi
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 # client
 client_cid=`docker ps --all --quiet --filter "name=runner_client"`
 #docker exec ${client_cid} sh -c "cd app/test && ./run.sh ${*}"
 #client_status=$?
 client_status=0
+
 #docker cp ${client_cid}:/tmp/coverage ${my_dir}/client
 # Client Coverage is broken. Simplecov is not seeing the *_test.rb files
 #echo "Coverage report copied to ${my_dir}/client/coverage"
 #cat ${my_dir}/client/coverage/done.txt
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-show_cids() {
+if [ ${client_status} != 0 ]; then
   echo
   echo "server: cid = ${server_cid}, status = ${server_status}"
   echo "client: cid = ${client_cid}, status = ${client_status}"
   echo
-}
+  exit 1
+fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo
+echo "All passed. Removing runner_client and runner_server containers..."
 
-if [ ${client_status} != 0 ] || [ ${server_status} != 0 ]; then
-  show_cids
-  exit 1
-else
-  echo
-  echo "All passed. Removing runner_client and runner_server containers..."
-  docker-compose down 2>/dev/null
-fi
+docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
+export DOCKER_ENGINE_VERSION=${docker_engine_version}
+docker-compose down 2>/dev/null
