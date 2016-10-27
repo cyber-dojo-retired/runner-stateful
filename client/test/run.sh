01b0667e647e55edf934b7ae96e5d54e5ac28469
Jon Jagger <jon@jaggersoft.com>
2016-10-27 13:31:29 +0100

    fix client run.sh to see hex-id args passed in

--- client/test/run.sh
+++ client/test/run.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# This is the only shell script that needs bash rather than sh
-# It needs it for the (array) handling below.
-# Called from test.sh (from outside the docker-container)
-
 if [ ! -f /.dockerenv ]; then
   echo 'FAILED: run.sh is being executed outside of docker-container.'
   echo 'Use test.sh which first calls build.sh'
   exit 1
 fi
 
-#cov_dir=/tmp/coverage
-#mkdir ${cov_dir}
-#test_log=${cov_dir}/test.log
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+rm -rf /tmp/cyber-dojo
+mkdir /tmp/cyber-dojo
+
+cov_dir=/tmp/coverage
+rm -rf ${cov_dir}
+mkdir ${cov_dir}
+test_log=${cov_dir}/test.log
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${my_dir}/src
-ruby -e "require './runner_test.rb'"
-
-#files=(*_test.rb)
-
-#args=(${*})
-
-#ruby -e "%w( ${files[*]} ).map{ |file| './'+file }.each { |file| require file }"
-#     -- ${args[@]} | tee ${test_log}
-
+files=(*_test.rb)
+args=(${*})
+ruby -e "%w( ${files[*]} ).shuffle.each{ |file| require './'+file }" -- ${args[@]} | tee ${test_log}
 #cd ${my_dir} && ruby ./check_test_results.rb ${test_log} ${cov_dir}/index.html > ${cov_dir}/done.txt
