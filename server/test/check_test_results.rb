e09c2164d6b275d948375ea6adeb3de45ac261ca
Jon Jagger <jon@jaggersoft.com>
2016-10-28 17:37:28 +0100

    tweak coverage limits

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -101,9 +101,9 @@ table =
     [ 'duration(test)[s]',      test_duration,      '<=',  45 ],
     [ 'coverage(src)[%]',       src_coverage,       '>=', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '>=', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 125 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 100 ],
     [ 'hits_per_line(test)',    hits_per_line_test, '<=',   5 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.9 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=',   2 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
