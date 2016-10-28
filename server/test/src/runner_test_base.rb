67e1a239335cb7de9307b028dc47f43d32a8a68a
Jon Jagger <jon@jaggersoft.com>
2016-10-28 15:14:25 +0100

    refactoring

--- server/test/src/runner_test_base.rb
+++ server/test/src/runner_test_base.rb
@@ -1,6 +1,5 @@
 # coverage require must come first
 require_relative '../coverage'
-require 'minitest/autorun'
 require_relative '../hex_mini_test'
 require_relative '../../src/micro_service'
 require_relative './docker_runner_helpers'
