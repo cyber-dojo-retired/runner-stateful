4ab50c3dc069ebe3ae0ba1a411067d054e9e56ca
Jon Jagger <jon@jaggersoft.com>
2016-10-28 21:22:54 +0100

    refactoring; improved hex-mini-test validate methods

--- server/test/hex_mini_test.rb
+++ server/test/hex_mini_test.rb
@@ -52,37 +52,25 @@ class HexMiniTest < MiniTest::Test
   # - - - - - - - - - - - - - - - - - - - - - -
 
   def self.validate_hex_prefix
-    spacer = "\n\n         !"
-    diagnostic = [
-      '',
-      '         !',
-      "def self.hex_prefix",
-      '',
-      ''
-    ].join("\n")
-
-    fail "#{spacer}missing#{diagnostic}" unless self.respond_to?(:hex_prefix)
-    fail "#{spacer}empty#{diagnostic}" if hex_prefix == ''
-    fail "#{spacer}not hex#{diagnostic}" unless hex_prefix =~ /^[0-9A-F]+$/
+    method = 'def self.hex_prefix'
+    pointer = ' ' * method.index('.') + '!'
+    pointee = (['',pointer,method,'','']).join("\n")
+    fail "\n\n#{pointer}missing#{pointee}" unless self.respond_to?(:hex_prefix)
+    fail "\n\n#{pointer}empty#{pointee}" if hex_prefix == ''
+    fail "\n\n#{pointer}not hex#{pointee}" unless hex_prefix =~ /^[0-9A-F]+$/
   end
 
   # - - - - - - - - - - - - - - - - - - - - - -
 
   def self.validate_hex_id(hex_suffix, lines)
+    method = "test '#{hex_suffix}',"
     proposition = lines.join(space = ' ')
-    spacer = "\n\n      !"
-    diagnostic = [
-      '',
-      '      !',
-      "test '#{hex_suffix}',",
-      "'#{proposition}'",
-      '',
-      ''
-    ].join("\n")
+    pointer = ' ' * method.index("'") + '!'
+    pointee = ['',pointer,method,"'#{proposition}'",'',''].join("\n")
     hex_id = hex_prefix + hex_suffix
-    fail "#{spacer}empty#{diagnostic}" if hex_suffix == ''
-    fail "#{spacer}not hex#{diagnostic}" unless hex_suffix =~ /^[0-9A-F]+$/
-    fail "#{spacer}duplicate#{diagnostic}" if @@seen_hex_ids.include?(hex_id)
+    fail "\n\n#{pointer}empty#{pointee}" if hex_suffix == ''
+    fail "\n\n#{pointer}not hex#{pointee}" unless hex_suffix =~ /^[0-9A-F]+$/
+    fail "\n\n#{pointer}duplicate#{pointee}" if @@seen_hex_ids.include?(hex_id)
   end
 
 end
