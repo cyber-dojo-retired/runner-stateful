d27500df0cff73fd49348522cfdb7483e11fbbd1
Jon Jagger <jon@jaggersoft.com>
2016-10-29 22:30:28 +0100

    [ci skip] notes update

--- server/test/src/runner_test_base.rb
+++ server/test/src/runner_test_base.rb
@@ -1,9 +1,9 @@
-# coverage require must come first
+# coverage must come first
 require_relative '../coverage'
 require_relative '../hex_mini_test'
 require_relative '../../src/micro_service'
+# docker_runner_helpers must come last
 require_relative './docker_runner_helpers'
-require 'json'
 
 class RunnerTestBase < HexMiniTest
 
