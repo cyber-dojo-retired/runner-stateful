ff7f1d2803f10689b5c084d7c41691c96d80214e
Jon Jagger <jon@jaggersoft.com>
2016-10-29 13:43:44 +0100

    add amber and green sections to demo's html

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -6,20 +6,16 @@ require_relative './runner_service_adapter'
 class Demo < Sinatra::Base
 
   get '/' do
-    kata_id = 'D4C8A65D61'
-    avatar_name = 'lion'
-    image_name = 'cyberdojofoundation/gcc_assert'
-    max_seconds = 10
-    deleted_filenames = []
-    changed_files = {
-      'hiker.c'       => read('hiker.c'),
+    hiker_c = read('hiker.c')
+    files = {
+      'hiker.c'       => hiker_c,
       'hiker.h'       => read('hiker.h'),
       'hiker.tests.c' => read('hiker.tests.c'),
       'cyber-dojo.sh' => read('cyber-dojo.sh'),
       'makefile'      => read('makefile')
     }
-    html = '<div style="font-size:0.5em">'
     json = nil
+    html = '<div style="font-size:0.5em">'
 
     duration = timed { json = runner.pulled?(image_name) }
     html += pre('pulled?', duration, json)
@@ -30,17 +26,32 @@ class Demo < Sinatra::Base
     duration = timed { json = runner.hello(kata_id, avatar_name) }
     html += pre('hello', duration, json)
 
-    duration = timed { json =
-      runner.run(image_name, kata_id, avatar_name, max_seconds, deleted_filenames, changed_files)
-    }
+    duration = timed { json = run(files) }
     html += pre('run', duration, json, 'red')
+
+    duration = timed { json = run({ 'hiker.c' => 'sdfsdfsdf'}) }
+    html += pre('run', duration, json, 'yellow')
+
+    duration = timed { json = run({ 'hiker.c' => hiker_c.sub('6 * 9', '6 * 7') }) }
+    html += pre('run', duration, json, 'green')
 
     duration = timed { json = runner.goodbye(kata_id, avatar_name) }
     html += pre('goodbye', duration, json)
+
     html += '</div>'
   end
 
   private
+
+  def kata_id; 'D4C8A65D61'; end
+  def avatar_name; 'lion'; end
+  def image_name; 'cyberdojofoundation/gcc_assert'; end
+  def max_seconds; 10; end
+  def deleted_filenames; []; end
+
+  def run(files)
+    runner.run(image_name, kata_id, avatar_name, max_seconds, deleted_filenames, files)
+  end
 
   def runner
     RunnerServiceAdapter.new
