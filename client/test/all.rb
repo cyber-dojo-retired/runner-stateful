28ca24004d2a3849682ba233adc23a64a8efd895
Jon Jagger <jon@jaggersoft.com>
2016-10-31 13:22:13 +0000

    refactoring

--- client/test/all.rb
+++ client/test/all.rb
@@ -1,4 +1,6 @@
+require 'json'
+require 'net/http'
 # This line must come first, before any required/loaded files to be covered.
 require_relative './coverage'
-require_relative './base'
+require_relative './test_base'
 require_relative './../src/runner_service_adapter'
---
+++ client/test/test_base.rb
@@ -0,0 +1,5 @@
+require_relative './hex_mini_test'
+
+class TestBase < HexMiniTest
+
+end
