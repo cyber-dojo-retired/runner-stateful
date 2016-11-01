dfb44efb707c421428330273674fe7481ae7d0e6
Jon Jagger <jon@jaggersoft.com>
2016-11-01 18:49:35 +0000

    refactoring; delete dead file

--- server/src/nearest_external.rb
+++ server/src/nearest_external.rb
@@ -15,10 +15,10 @@ end
 # Assumes the object (which included the module) has a parent and
 # repeatedly chains back parent to parent to parent until it gets
 # to an object with the required property, or runs out of parents.
-# Properties accessed in this way include:
+# Properties accessed in this way:
 #   o) shell - executes shell commands
 #   o) disk  - file-system directories and file read/write
-#   o) log   - memory/stdout based logging
+#   o) log   - stdout based logging
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Allows classes representing external objects to easily access
 # each other as well.
