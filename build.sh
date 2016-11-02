768ba7f609358d37abae11e43091d668bcdbf5c8
Jon Jagger <jon@jaggersoft.com>
2016-11-02 13:07:03 +0000

    base client and server images on new cyberdojo/foreman image

--- build.sh
+++ build.sh
@@ -12,6 +12,5 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
 export DOCKER_ENGINE_VERSION=${docker_engine_version}
 
-docker-compose -f ${my_dir}/base/docker-compose.yml build
 docker-compose -f ${my_dir}/server/docker-compose.yml build
 docker-compose -f ${my_dir}/client/docker-compose.yml build
