6c170eada64cf024c3b9489e5939e9ad44dbcd76
Jon Jagger <jon@jaggersoft.com>
2016-11-12 06:38:00 +0000

    drop requirement that test's hex-id has to be 8 chars long

--- server/test/src/runner_test_base.rb
+++ server/test/src/runner_test_base.rb
@@ -127,11 +127,7 @@ class RunnerTestBase < HexMiniTest
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  def kata_id; test_id + '0' * (10-test_id.length); end
   def avatar_name; 'salmon'; end
-
-  def kata_id;
-    assert_equal 8, test_id.length, 'test_id.length'
-    test_id + '00'
-  end
 
 end
