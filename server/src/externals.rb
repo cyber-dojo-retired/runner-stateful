2afcdbd025450e1d655fc5ca81bdc6a9208f7b93
Jon Jagger <jon@jaggersoft.com>
2016-11-12 06:13:14 +0000

    added documentation on design of Externals<->NearestExternal

--- server/src/externals.rb
+++ server/src/externals.rb
@@ -9,3 +9,47 @@ module Externals
   def   log;   @log ||= ExternalStdoutLogger.new(self); end
 
 end
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# How does Externals work? How are they designed to be used?
+#
+# 1. include Externals in your top-level scope.
+#
+#    require_relative './externals'
+#    class MicroService < Sinatra::Base
+#      ...
+#      private
+#      include Externals
+#      def runner; DockerRunner.new(self); end
+#      ...
+#    end
+#
+# 2. All child objects have access to their parent
+#    and gain access to the externals via nearest_external()
+#
+#    require_relative './nearest_external'
+#    class DockerRunner
+#      def initialize(parent)
+#        @parent = parent
+#      end
+#      attr_reader :parent
+#      ...
+#      private
+#      include NearestExternal
+#      def log; nearest_external(:log); end
+#      ...
+#    end
+#
+# 3. In tests you can simply set the external directly.
+#    Note that Externals.log uses ||=
+#
+#    class ExampleTest < MiniTest::Test
+#      def test_something
+#        @log = SpyLogger.new(...)
+#        runner = DockerRunner.new(self)
+#        runner.do_something
+#        assert_equal 'expected', @log.spied
+#      end
+#    end
+#
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
