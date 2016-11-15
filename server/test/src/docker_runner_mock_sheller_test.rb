6f312c1eaae36d2fa9a2d6b98c7adf2f6fae9012
Jon Jagger <jon@jaggersoft.com>
2016-11-15 16:15:02 +0000

    move DockerRunner tests that use MockSheller into their own test file as I can't see an easy way, in the same file, to make some tests see real external shell, and some tests see mock-shell

--- server/test/src/docker_runner_mock_sheller_test.rb
+++ server/test/src/docker_runner_mock_sheller_test.rb
@@ -1,7 +1,7 @@
 require_relative './runner_test_base'
 require_relative './mock_sheller'
 
-class DockerRunnerPullMockShellerTest < RunnerTestBase
+class DockerRunnerMockShellerTest < RunnerTestBase
 
   def self.hex_prefix; '0D5'; end
 
@@ -51,6 +51,37 @@ class DockerRunnerPullMockShellerTest < RunnerTestBase
     assert_equal '', stdout
     assert_equal '', stderr
     assert_equal success, status
+    shell.teardown
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '36C',
+  'when image_name is valid and has not been pulled',
+  'then new_kata(image_name, kata_id) pulls it' do
+    image_name = 'cdf/ruby_mini_test'
+    stdout = [
+      'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
+      'cdf/gcc_assert latest 28683e525ad3 9 days ago 95.97 MB'
+    ].join("\n")
+    shell.mock_exec('docker images', stdout, '', 0)
+    shell.mock_exec("docker pull #{image_name}", '','',0)
+    runner.new_kata(image_name, kata_id)
+    shell.teardown
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'DFA',
+  'when image_name is valid has been pulled',
+  'then new_kata(image_name, kata_id) does not pull it' do
+    image_name = 'cdf/gcc_assert'
+    stdout = [
+      'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
+      "#{image_name}  latest 28683e525ad3 9 days ago 95.97 MB"
+    ].join("\n")
+    shell.mock_exec('docker images', stdout, '', 0)
+    runner.new_kata(image_name, kata_id)
     shell.teardown
   end
 
