19a7641cf0d1ffc80e61265e15bc763d1978f77b
Jon Jagger <jon@jaggersoft.com>
2016-11-15 15:23:53 +0000

    added test for spy_logger

---
+++ server/test/src/spy_logger_test.rb
@@ -0,0 +1,18 @@
+require_relative './runner_test_base'
+require_relative './spy_logger'
+
+class SpyLoggerTest < RunnerTestBase
+
+  def self.hex_prefix; 'CD4'; end
+
+  test '20C',
+  'logged message is spied' do
+    logger = SpyLogger.new(nil)
+    assert_equal [], logger.spied
+    logger << 'hello'
+    assert_equal ['hello'], logger.spied
+    logger << 'world'
+    assert_equal ['hello','world'], logger.spied
+  end
+
+end
