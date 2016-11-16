b72748f9b907ae6b2dbacd58f2f615683de7f115
Jon Jagger <jon@jaggersoft.com>
2016-11-16 17:40:19 +0000

    use mocked shell.exec in more tests to improve testing experience when you have no network connectivity

--- server/test/src/docker_runner_kata_test.rb
+++ server/test/src/docker_runner_kata_test.rb
@@ -7,21 +7,6 @@ class DockerRunnerKataTest < RunnerTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'AED',
-  'when image_name is invalid then new_kata(image_name, kata_id) fails with not-found' do
-    bad_image_name = '123/123'
-    runner.logging_off
-    raised = assert_raises(DockerRunnerError) { runner.new_kata(bad_image_name, kata_id) }
-    refute_equal 0, raised.status
-    assert_equal [
-      "Using default tag: latest",
-      "Pulling repository docker.io/#{bad_image_name}"
-    ].join("\n"), raised.stdout
-    assert_equal "Error: image #{bad_image_name}:latest not found", raised.stderr
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - -
-
   test 'FA0',
   "[gcc,assert] old_kata removes all avatar's volumes" do
     @image_name = 'cyberdojofoundation/gcc_assert'
--- server/test/src/docker_runner_mock_sheller_test.rb
+++ server/test/src/docker_runner_mock_sheller_test.rb
@@ -8,9 +8,26 @@ class DockerRunnerMockShellerTest < RunnerTestBase
   def shell; @shell ||= MockSheller.new(nil); end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # pulled?
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'D97',
+  'when image_name is invalid, pulled?(image_name) does not raise and result is false' do
+    image_name = '123/123'
+    stdout = [
+      'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
+      'cdf/gcc_assert latest 28683e525ad3 9 days ago 95.97 MB'
+    ].join("\n")
+    shell.mock_exec('docker images', stdout, '', 0)
+    runner.logging_off
+    refute runner.pulled?(image_name)
+    shell.teardown
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '9C3',
-  'pulled?(image_name) is false' do
+  'when image_name is valid, pulled?(image_name) and is false' do
     image_name = 'cdf/ruby_mini_test'
     stdout = [
       'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
@@ -25,7 +42,7 @@ class DockerRunnerMockShellerTest < RunnerTestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test 'A44',
-  'pulled?(image_name) is true' do
+  'when image_name is valid, pulled?(image_name) is true' do
     image_name = 'cdf/ruby_mini_test'
     stdout = [
       'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
@@ -38,12 +55,68 @@ class DockerRunnerMockShellerTest < RunnerTestBase
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # pull
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'A73',
+  'when image_name is invalid, pulled?(image_name) raises DockerRunnerError' do
+    image_name = '123/123'
+    stdout = [
+      'Using default tag: latest',
+      "Pulling repository docker.io/#{image_name}"
+    ].join("\n")
+    stderr = "Error: image #{image_name}:latest not found"
+    shell.mock_exec("docker pull #{image_name}", stdout, stderr, 1)
+    runner.logging_off
+    raised = assert_raises(DockerRunnerError) { runner.pull(image_name) }
+    assert_equal 1, raised.status
+    assert_equal stdout, raised.stdout
+    assert_equal stderr, raised.stderr
+    shell.teardown
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '91C',
-  'pull(image_name) issues unconditional docker-pull command' do
+  'when image_name is valud, pull(image_name) issues unconditional docker-pull' do
     image_name = 'cdf/ruby_mini_test'
     shell.mock_exec("docker pull #{image_name}", '', '', success)
     runner.pull(image_name)
+    shell.teardown
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # new_kata
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # test '933', no network connectivitity
+  #stderr = [
+  # "Error while pulling image: Get",
+  # "https://index.docker.io/v1/repositories/123/123/images:"
+  # "dial tcp: lookup index.docker.io on 10.0.2.3:53: no such host"
+  # ].join(' ')
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'AED',
+  'when image_name is invalid, then new_kata(image_name, kata_id) fails with not-found' do
+    bad_image_name = '123/123'
+    runner.logging_off
+    stdout = [
+      'REPOSITORY     TAG    IMAGE ID     CREATED    SIZE',
+      'cdf/gcc_assert latest 28683e525ad3 9 days ago 95.97 MB'
+    ].join("\n")
+    shell.mock_exec('docker images', stdout, '', 0)
+    stdout = [
+      "Using default tag: latest",
+      "Pulling repository docker.io/#{bad_image_name}"
+    ].join("\n")
+    stderr = "Error: image #{bad_image_name}:latest not found"
+    shell.mock_exec("docker pull #{bad_image_name}", stdout, stderr, 1)
+    raised = assert_raises(DockerRunnerError) { runner.new_kata(bad_image_name, kata_id) }
+    assert_equal 1, raised.status
+    assert_equal stdout, raised.stdout
+    assert_equal stderr, raised.stderr
     shell.teardown
   end
 
