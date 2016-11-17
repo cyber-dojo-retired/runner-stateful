b6cb8e288e909b20c29ad8f12a9d47cf51de695b
Jon Jagger <jon@jaggersoft.com>
2016-11-17 13:16:06 +0000

    added docker-compose is installed check to build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
+hash docker-compose 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker-compose is not installed"
+  exit 1
+fi
+
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
 export DOCKER_ENGINE_VERSION=${docker_engine_version}
