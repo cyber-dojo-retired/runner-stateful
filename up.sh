a57c939d3db9a555b8ebf265d60eb3f8b1a92437
Jon Jagger <jon@jaggersoft.com>
2016-11-26 21:34:26 +0000

    delete check from up.sh that build.sh already does

--- up.sh
+++ up.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-hash docker 2> /dev/null
-if [ $? != 0 ]; then
-  echo
-  echo "docker is not installed"
-  exit 1
-fi
-
-docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
-export DOCKER_ENGINE_VERSION=${docker_engine_version}
-
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 docker-compose --file ${my_dir}/docker-compose.yml down
 docker-compose --file ${my_dir}/docker-compose.yml up -d
