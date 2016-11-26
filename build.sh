df84909b6c4b29d013aa2007d7b597a97e52e298
Jon Jagger <jon@jaggersoft.com>
2016-11-26 21:30:54 +0000

    base Dockerfile on cyberdojo/docker

--- build.sh
+++ build.sh
@@ -15,8 +15,6 @@ if [ $? != 0 ]; then
 fi
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker_engine_version=$(docker --version | awk '{print $3}' | sed '$s/.$//')
-export DOCKER_ENGINE_VERSION=${docker_engine_version}
 
 docker-compose --file ${my_dir}/server/docker-compose.yml build
 docker-compose --file ${my_dir}/client/docker-compose.yml build
