b457327d6caeb3c70993300d807e4132a5b969af
Jon Jagger <jon@jaggersoft.com>
2016-12-22 05:00:09 +0000

    break lone lines in bash script using backslash line continuation

--- client/test/run.sh
+++ client/test/run.sh
@@ -21,5 +21,9 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${my_dir}/src
 files=(*_test.rb)
 args=(${*})
-ruby -e "([ '../coverage.rb' ] + %w(${files[*]}).shuffle).each{ |file| require './'+file }" -- ${args[@]} | tee ${test_log}
-cd ${my_dir} && ruby ./check_test_results.rb ${test_log} ${cov_dir}/index.html > ${cov_dir}/done.txt
+
+ruby -e "([ '../coverage.rb' ] + %w(${files[*]}).shuffle).each{ |file| require './'+file }" \
+  -- ${args[@]} | tee ${test_log}
+
+cd ${my_dir} \
+  && ruby ./check_test_results.rb ${test_log} ${cov_dir}/index.html > ${cov_dir}/done.txt
