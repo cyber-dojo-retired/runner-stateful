160b6e386bc437c3ea8166f8e4c841c8bee994bc
Jon Jagger <jon@jaggersoft.com>
2016-12-31 13:32:24 +0000

    added more missing client-side tests

--- client/test/src/avatar_test.rb
+++ client/test/src/avatar_test.rb
@@ -40,6 +40,21 @@ class AvatarTest < TestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - -
 
+  test '1E0',
+  'new_avatar with kata_id that exists and avatar_name that exists raises' do
+    new_avatar
+    begin
+      error = assert_raises {
+        new_avatar(image_name, kata_id, avatar_name)
+      }
+      assert_equal 'RunnerService:new_avatar:avatar_name:exists', error.message
+    ensure
+      old_avatar
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
   test '70F',
   'old_avatar with invalid kata_id raises' do
     error = assert_raises {
@@ -56,6 +71,16 @@ class AvatarTest < TestBase
       old_avatar(image_name, kata_id.reverse, 'salmon')
     }
     assert_equal 'RunnerService:old_avatar:kata_id:!exists', error.message
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'DE2',
+  'old_avatar with kata_id that exists and avatar_name that does not exist raises' do
+    error = assert_raises {
+      old_avatar(image_name, kata_id, 'salmon')
+    }
+    assert_equal 'RunnerService:old_avatar:avatar_name:!exists', error.message
   end
 
 end
