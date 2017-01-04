6158079ab64ba738b0fbf67275dbcbb7f5f1b933
Jon Jagger <jon@jaggersoft.com>
2017-01-04 15:52:11 +0000

    added reminder that remove_volumes.sh needs reworking

--- remove_volumes.sh
+++ remove_volumes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker volume rm $(docker volume ls --quiet --filter 'name=cyber_dojo_')
+echo "NEEDS REFACTORING NOW VOLUMES COME FROM DATA-ONLY CONTAINERS...."
+#docker volume rm $(docker volume ls --quiet --filter 'name=cyber_dojo_')
