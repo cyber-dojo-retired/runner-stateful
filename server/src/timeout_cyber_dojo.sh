5af7e527ba156118f232ab71f646b14432af1d80
Jon Jagger <jon@jaggersoft.com>
2017-01-08 09:35:45 +0000

    fixed stdout/stderr piping to /dev/null in timeout script; old way failed some tests

--- server/src/timeout_cyber_dojo.sh
+++ server/src/timeout_cyber_dojo.sh
@@ -11,13 +11,13 @@ export CYBER_DOJO_SANDBOX=/sandboxes/${AVATAR}
 
 cd ${CYBER_DOJO_SANDBOX}
 
-grep -q -c Alpine /etc/issue &>/dev/null
+grep -q -c Alpine /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   su ${AVATAR} -p -c "timeout -s KILL -t ${MAX_SECONDS} sh ./cyber-dojo.sh"
   status=$?
 fi
 
-grep -q -c Ubuntu /etc/issue &>/dev/null
+grep -q -c Ubuntu /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   su ${AVATAR} -p -c "timeout -s KILL ${MAX_SECONDS}s sh ./cyber-dojo.sh"
   status=$?
