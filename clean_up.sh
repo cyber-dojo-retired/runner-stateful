2c1ee95354bb47f2e6ca73dee95f143cdfaecfb5
Jon Jagger <jon@jaggersoft.com>
2017-01-08 15:21:09 +0000

    added comment to clean-up script re how collector will work

--- clean_up.sh
+++ clean_up.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker rm --force --volumes $(docker ps --quiet --filter 'name=cyber_dojo_')
+
+# collector can do this
+# - - - - - - - - - - -
+# pids=$(docker ps --quiet --filter 'status=exited' --filter 'name=cyber_dojo_')
+# echo ${pids} | xargs docker rm --force --volumes
