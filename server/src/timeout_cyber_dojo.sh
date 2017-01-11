2f380dc11fd58dfed339cf396ae5ddc24aab8260
Jon Jagger <jon@jaggersoft.com>
2017-01-11 10:18:40 +0000

    altered timeout script so an avatar's processes are explicitly killed on both Alpine and Ubunu image containers; belt-and-braces for Ubuntu whose timeout command seems to kill the whole process tree anyway

--- server/src/timeout_cyber_dojo.sh
+++ server/src/timeout_cyber_dojo.sh
@@ -14,17 +14,18 @@ cd ${CYBER_DOJO_SANDBOX}
 
 grep -q -c Alpine /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
+  # On Alpine's ps, the user's name is truncated to 8 chars
+  PS_AVATAR=`echo ${AVATAR} | cut -c -8`
   su ${AVATAR} -p -c "timeout -s KILL -t ${MAX_SECONDS} sh ./cyber-dojo.sh"
   status=$?
 fi
 
 grep -q -c Ubuntu /etc/issue >/dev/null 2>&1
 if [ $? -eq 0 ]; then
+  PS_AVATAR=${AVATAR}
   su ${AVATAR} -p -c "timeout -s KILL ${MAX_SECONDS}s sh ./cyber-dojo.sh"
   status=$?
 fi
 
-# On Alpine's ps, the user's name is truncated to 8 chars
-SHORT_AVATAR=`echo ${AVATAR} | cut -c -8`
-ps -o user,pid | grep "^${SHORT_AVATAR}\s" | awk '{print $2}' | xargs kill -9
+ps -o user,pid | grep "^${PS_AVATAR}\s" | awk '{print $2}' | xargs kill -9
 exit ${status}
