bf9d725c03584926f31ec6e5b365f60d51de0af1
Jon Jagger <jon@jaggersoft.com>
2017-01-14 08:38:17 +0000

    added --volumes to [docker rm] in cleanup script; it's a no-op if the kata-container does not use explicit volumes, it gc's if they do - they could easily get added if volumes are a way to add disk-quotas

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=cyber_dojo_kata_container_runner_
-docker rm --force $(docker ps --all --quiet --filter "name=${NAME}")
+docker rm --force --volumes $(docker ps --all --quiet --filter "name=${NAME}")
 
 NAME=cyber_dojo_kata_volume_runner_
 docker rm --force --volumes $(docker ps --all --quiet --filter "name=${NAME}")
