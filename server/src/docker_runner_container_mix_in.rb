d832a704d42c7629c088011e2c51149b19e6105e
Jon Jagger <jon@jaggersoft.com>
2017-01-15 14:24:55 +0000

    refactoring; move mix-in methods

--- server/src/docker_runner_container_mix_in.rb
+++ server/src/docker_runner_container_mix_in.rb
@@ -3,37 +3,5 @@ module DockerRunnerContainerMixIn
 
   module_function # = = = = = = = = = = = = = = = =
 
-  def alpine_add_group_cmd
-    "addgroup -g #{gid} #{group}"
-  end
-
-  def ubuntu_add_group_cmd
-    "addgroup --gid #{gid} #{group}"
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def fail_kata_id(message)
-    fail bad_argument("kata_id:#{message}")
-  end
-
-  def fail_avatar_name(message)
-    fail bad_argument("avatar_name:#{message}")
-  end
-
-  def fail_command(message)
-    fail bad_argument("command:#{message}")
-  end
-
-  def bad_argument(message)
-    ArgumentError.new(message)
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - -
-
-  def sandboxes_root; '/sandboxes'; end
-  def success; shell.success; end
-  def space; ' '; end
-
 end
 
--- server/src/docker_runner_mix_in.rb
+++ server/src/docker_runner_mix_in.rb
@@ -95,6 +95,16 @@ module DockerRunnerMixIn
 
   # - - - - - - - - - - - - - - - - - - - - - - - -
 
+  def alpine_add_group_cmd
+    "addgroup -g #{gid} #{group}"
+  end
+
+  def ubuntu_add_group_cmd
+    "addgroup --gid #{gid} #{group}"
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
   def alpine_add_user_cmd(avatar_name)
     home = home_path(avatar_name)
     uid = user_id(avatar_name)
@@ -156,6 +166,24 @@ module DockerRunnerMixIn
 
   # - - - - - - - - - - - - - - - - - - - - - - - -
 
+  def fail_kata_id(message)
+    fail bad_argument("kata_id:#{message}")
+  end
+
+  def fail_avatar_name(message)
+    fail bad_argument("avatar_name:#{message}")
+  end
+
+  def fail_command(message)
+    fail bad_argument("command:#{message}")
+  end
+
+  def bad_argument(message)
+    ArgumentError.new(message)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
   def assert_exec(cmd)
     stdout,stderr,status = exec(cmd)
     unless status == success
@@ -177,6 +205,10 @@ module DockerRunnerMixIn
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def sandboxes_root; '/sandboxes'; end
+  def success; shell.success; end
+  def space; ' '; end
 
   include NearestAncestors
   def shell; nearest_ancestors(:shell); end
