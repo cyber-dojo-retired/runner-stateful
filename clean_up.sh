cc11e96b3588add79b3ccc619762915c8d907310
Jon Jagger <jon@jaggersoft.com>
2017-01-18 16:07:30 +0000

    added comments to clean-up script

--- clean_up.sh
+++ clean_up.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Simple brute-force script to remove all local runner volumes
+# Useful during development when tests fails and leave
+# volumes behind, whose mere existence then interferes with
+# subsequent test runs.
+
 # The Kata-Container-Runner creates (per kata) a long-lived
 # named container.
 NAME=cyber_dojo_kata_container_runner_
@@ -9,14 +14,9 @@ docker rm --force --volumes $(docker ps --all --quiet --filter "name=${NAME}")
 NAME=cyber_dojo_kata_volume_runner_
 docker volume rm $(docker volume ls --quiet --filter "name=${NAME}")
 
-
 # The Avatar-Volume-Runner creates a named volume per kata and per avatar
 NAME=cyber_dojo_avatar_volume_runner_kata_
 docker volume rm $(docker volume ls --quiet --filter "name=${NAME}")
 
 NAME=cyber_dojo_avatar_volume_runner_avatar_
 docker volume rm $(docker volume ls --quiet --filter "name=${NAME}")
-
-
-# pids=$(docker ps --all --quiet --filter 'status=exited' --filter 'name=...')
-# echo ${pids} | xargs docker rm --force
