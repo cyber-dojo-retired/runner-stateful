36718ac59bc097b2a05d511e6b2331ef8084fb42
Jon Jagger <jon@jaggersoft.com>
2017-01-29 18:53:30 +0000

    refactoring; made image_name,kata_id ctor parameters and removed from all server methods

---
+++ server/test/src/runner_test.rb
@@ -0,0 +1,40 @@
+require_relative 'test_base'
+
+class RunnerTest < TestBase
+
+  def self.hex_prefix; '4C8DB'; end
+
+  test 'D01',
+  'runner with valid_kata is does not raise' do
+    kata_id = '4C8DBD014C'
+    runner(image_name, kata_id)
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '6FD',
+  'runner with invalid kata_id raises' do
+    invalid_kata_ids.each do |invalid_kata_id|
+      error = assert_raises(ArgumentError) {
+        @runner = nil
+        runner(image_name, invalid_kata_id)
+      }
+      assert_equal 'kata_id:invalid', error.message
+    end
+  end
+
+  private
+
+  def invalid_kata_ids
+    [
+      nil,          # not string
+      Object.new,   # not string
+      [],           # not string
+      '',           # not 10 chars
+      '123456789',  # not 10 chars
+      '123456789AB',# not 10 chars
+      '123456789G'  # not 10 hex-chars
+    ]
+  end
+
+end
