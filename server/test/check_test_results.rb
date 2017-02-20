0b91b6f6d4a95e702ab74a4abd04ca46950e079d
Jon Jagger <jon@jaggersoft.com>
2017-02-20 11:13:07 +0000

    increased max-duration for server tests - more tests and Travis seems slower

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -98,7 +98,7 @@ table =
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
     [ 'assertions/s',           assertions_per_sec, '>=',   1 ],
-    [ 'duration(test)[s]',      test_duration,      '<=', 150 ],
+    [ 'duration(test)[s]',      test_duration,      '<=', 210 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
     [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 300 ],
