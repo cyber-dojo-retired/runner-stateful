c4197917a09c1ebea8c98cfcc20d826dd9cc57c6
Jon Jagger <jon@jaggersoft.com>
2017-02-20 12:19:50 +0000

    refactoring; collapse run-alpine/run-ubuntu test duplication

--- server/test/src/run_os_test.rb
+++ server/test/src/run_os_test.rb
@@ -5,81 +5,90 @@ class RunAlpineTest < TestBase
 
   include OsHelper
 
-  def self.hex_prefix; '4D778'; end
+  def self.hex_prefix; '3759D'; end
 
   def hex_setup; kata_setup; end
   def hex_teardown; kata_teardown; end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '997',
-  '[Alpine] container has access to cyber-dojo env-vars' do
+  def self.os_test(hex_suffix, *lines, &test_block)
+    alpine_lines = ['[Alpine]'] + lines
+    test(hex_suffix+'0', *alpine_lines, &test_block)
+    ubuntu_lines = ['[Ubuntu]'] + lines
+    test(hex_suffix+'1', *ubuntu_lines, &test_block)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  os_test '997',
+  'container has access to cyber-dojo env-vars' do
     kata_id_env_vars_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '582',
-  '[Alpine] has group used for dir/file ownership' do
+  os_test '582',
+  'has group used for dir/file ownership' do
     assert_group_exists
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '2A0',
-  '[Alpine] new_avatar has HOME set off /home' do
+  os_test '2A0',
+  'new_avatar has HOME set off /home' do
     new_avatar_home_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '0C9',
-  '[Alpine] new_avatar has its own sandbox with owner/group/permissions set' do
+  os_test '0C9',
+  'new_avatar has its own sandbox with owner/group/permissions set' do
     new_avatar_sandbox_setup_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '1FB',
-  '[Alpine] new_avatar has starting-files in its sandbox with owner/group/permissions set' do
+  os_test '1FB',
+  'new_avatar has starting-files in its sandbox with owner/group/permissions set' do
     new_avatar_starting_files_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '4E8',
-  '[Alpine] unchanged files still exist and are unchanged' do
+  os_test '4E8',
+  'unchanged files still exist and are unchanged' do
     unchanged_files_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '385',
-  '[Alpine] deleted files are removed',
+  os_test '385',
+  'deleted files are removed',
   'and all previous files are unchanged' do
     deleted_files_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '232',
-  '[Alpine] new files are added with owner/group/permissions',
+  os_test '232',
+  'new files are added with owner/group/permissions',
   'and all previous files are unchanged' do
     new_files_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '9A7',
-  '[Alpine] a changed file is resaved and its size and time-stamp updates',
+  os_test '9A7',
+  'a changed file is resaved and its size and time-stamp updates',
   'and all previous files are unchanged' do
     changed_file_test
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'D7C',
-  '[Alpine] is ulimited' do
+  os_test 'D7C',
+  'is ulimited' do
     ulimit_test
   end
 
