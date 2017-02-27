e402302855ec3712d502af011a86c8f53cba0e4d
Jon Jagger <jon@jaggersoft.com>
2017-02-27 09:11:55 +0000

    refactoring

--- server/src/micro_service.rb
+++ server/src/micro_service.rb
@@ -57,7 +57,7 @@ class MicroService < Sinatra::Base
     name = caller.to_s[prefix.length .. -1]
     { name => runner.send(name, *args) }.to_json
   rescue Exception => e
-    log << "EXCEPTION: #{e.class.name} #{e.to_s}"
+    log << "EXCEPTION: #{e.class.name}.#{caller} #{e.message}"
     { 'exception' => e.message }.to_json
   end
 
