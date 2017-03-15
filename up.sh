6ecb4822e97714021f4eefd5848ac64aa07f6385
Jon Jagger <jon@jaggersoft.com>
2017-03-15 08:07:00 +0000

    fixed up.sh which was leaking docker volumes; also added checks to exit up.sh if client/server containers exit on startup

--- up.sh
+++ up.sh
@@ -2,5 +2,23 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker-compose --file ${my_dir}/docker-compose.yml down
+docker-compose --file ${my_dir}/docker-compose.yml down --volumes
 docker-compose --file ${my_dir}/docker-compose.yml up -d
+
+sleep 1
+
+check_up()
+{
+  set +e
+  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
+  set -e
+  if [ "${s}" != "${1}" ]; then
+    echo
+    echo "${1} exited"
+    docker logs ${1}
+    exit 1
+  fi
+}
+
+check_up 'runner_stateless_server'
+check_up 'runner_stateless_client'
