cc6590bea133dc0bb67276a9e20c752f679b7f50
Jon Jagger <jon@jaggersoft.com>
2017-04-15 08:30:59 +0100

    refactoring; move global methods inside class

--- server/src/runner.rb
+++ server/src/runner.rb
@@ -16,40 +16,7 @@ def container_runner?(image_name)
   tagless(image_name).end_with?('shared_process')
 end
 
-def tagless(image_name)
-  o = split_image_name(image_name)
-  o[:name]
-end
-
-def split_image_name(image_name)
-  # http://stackoverflow.com/questions/37861791
-  i = image_name.index('/')
-  if i.nil? || i == -1 || (
-      !image_name[0...i].include?('.') &&
-      !image_name[0...i].include?(':') &&
-       image_name[0...i] != 'localhost')
-    hostname = ''
-    remote_name = image_name
-  else
-    hostname = image_name[0..i-1]
-    remote_name = image_name[i+1..-1]
-  end
-
-  alpha_numeric = '[a-z0-9]+'
-  separator = '([.]{1}|[_]{1,2}|[-]+)'
-  component = "#{alpha_numeric}(#{separator}#{alpha_numeric})*"
-  name = "#{component}(/#{component})*"
-  tag = '[\w][\w.-]{0,126}'
-  md = /^(#{name})(:(#{tag}))?$/.match(remote_name)
-
-  fail ArgumentError.new('image_name:invalid') if md.nil?
-
-  {
-    hostname:hostname,
-    name:md[1],
-    tag:md[8] || ''
-  }
-end
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 module Runner # mix-in
 
@@ -58,7 +25,43 @@ module Runner # mix-in
   end
 
   def new_runner(image_name, kata_id)
-    Object.const_get(runner_class_name(image_name)).new(self, image_name, kata_id)
+    class_name = runner_class_name(tagless(image_name))
+    Object.const_get(class_name).new(self, image_name, kata_id)
+  end
+
+  def tagless(image_name)
+    o = split_image_name(image_name)
+    o[:name]
+  end
+
+  def split_image_name(image_name)
+    # http://stackoverflow.com/questions/37861791
+    i = image_name.index('/')
+    if i.nil? || i == -1 || (
+        !image_name[0...i].include?('.') &&
+        !image_name[0...i].include?(':') &&
+         image_name[0...i] != 'localhost')
+      hostname = ''
+      remote_name = image_name
+    else
+      hostname = image_name[0..i-1]
+      remote_name = image_name[i+1..-1]
+    end
+
+    alpha_numeric = '[a-z0-9]+'
+    separator = '([.]{1}|[_]{1,2}|[-]+)'
+    component = "#{alpha_numeric}(#{separator}#{alpha_numeric})*"
+    name = "#{component}(/#{component})*"
+    tag = '[\w][\w.-]{0,126}'
+    md = /^(#{name})(:(#{tag}))?$/.match(remote_name)
+
+    fail ArgumentError.new('image_name:invalid') if md.nil?
+
+    {
+      hostname:hostname,
+      name:md[1],
+      tag:md[8] || ''
+    }
   end
 
 end
