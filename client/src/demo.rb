f76e6ab808bf67bedb4a89f3b61744fd6a514122
Jon Jagger <jon@jaggersoft.com>
2017-08-22 11:23:43 +0530

    demo refactoring

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -5,48 +5,96 @@ require 'sinatra/base'
 class Demo < Sinatra::Base
 
   get '/' do
-    hiker_c = read('hiker.c')
-    files = {
+    @html = ''
+    in_kata {
+      as_avatar {
+        red; amber; green; time_out
+      }
+    }
+    "<div style='font-size:0.5em'>#{@html}</div>"
+  end
+
+  private
+
+  def red
+    change = {}
+    duration = timed { @sss = run(change) }
+    @html += pre('run', duration, 'Red', @sss)
+  end
+
+  def amber
+    change = { 'hiker.c' => 'sdsdsdsd' }
+    duration = timed { @sss = run(change) }
+    @html += pre('run', duration, 'Yellow', @sss)
+  end
+
+  def green
+    change = { 'hiker.c' => hiker_c.sub('6 * 9', '6 * 7') }
+    duration = timed { @sss = run(change) }
+    @html += pre('run', duration, 'Lime', @sss)
+  end
+
+  def time_out
+    change = { 'hiker.c' => hiker_c.sub('return', "for(;;);\n return") }
+    duration = timed { @sss = run(change, 3) }
+    @html += pre('run', duration, 'LightGray', @sss)
+  end
+
+  def in_kata
+    duration = timed { runner.kata_new(image_name, kata_id) }
+    @html += pre('kata_new', duration)
+    begin
+      yield
+    ensure
+      duration = timed { runner.kata_old(image_name, kata_id) }
+      @html += pre('kata_old', duration)
+    end
+  end
+
+  def as_avatar
+    duration = timed { runner.avatar_new(image_name, kata_id, avatar_name, files) }
+    @html += pre('avatar_new', duration)
+    begin
+      yield
+    ensure
+      duration = timed { runner.avatar_old(image_name, kata_id, avatar_name) }
+      @html += pre('avatar_old', duration)
+    end
+  end
+
+  def timed
+    started = Time.now
+    yield
+    finished = Time.now
+    '%.2f' % (finished - started)
+  end
+
+  def runner
+    RunnerService.new
+  end
+
+  def run(files, max_seconds=10)
+    deleted_filenames = []
+    runner.run(image_name, kata_id, avatar_name, deleted_filenames, files, max_seconds)
+  end
+
+  def files
+    {
       'hiker.c'       => hiker_c,
       'hiker.h'       => read('hiker.h'),
       'hiker.tests.c' => read('hiker.tests.c'),
       'cyber-dojo.sh' => read('cyber-dojo.sh'),
       'makefile'      => read('makefile')
     }
-    sss = nil
-    html = '<div style="font-size:0.5em">'
-
-    duration = timed { runner.kata_new(image_name, kata_id) }
-    html += pre('kata_new', duration)
-
-    duration = timed { runner.avatar_new(image_name, kata_id, avatar_name, files) }
-    html += pre('avatar_new', duration)
-
-    duration = timed { sss = run({}) }
-    html += pre('run', duration, 'Red', sss)
-
-    syntax_error = { 'hiker.c' => 'sdsdsdsd' }
-    duration = timed { sss = run(syntax_error) }
-    html += pre('run', duration, 'Yellow', sss)
-
-    tests_run_and_pass = { 'hiker.c' => hiker_c.sub('6 * 9', '6 * 7') }
-    duration = timed { sss = run(tests_run_and_pass) }
-    html += pre('run', duration, 'Lime', sss)
-
-    times_out = { 'hiker.c' => hiker_c.sub('return', "for(;;);\n    return") }
-    duration = timed { sss = run(times_out, 3) }
-    html += pre('run', duration, 'LightGray', sss)
-
-    duration = timed { runner.avatar_old(image_name, kata_id, avatar_name) }
-    html += pre('avatar_old', duration)
-
-    duration = timed { runner.kata_old(image_name, kata_id) }
-    html += pre('kata_old', duration)
-
-    html += '</div>'
   end
 
-  private
+  def hiker_c
+    read('hiker.c')
+  end
+
+  def read(filename)
+    IO.read("/app/test/start_files/gcc_assert/#{filename}")
+  end
 
   def image_name
     'cyberdojofoundation/gcc_assert'
@@ -58,26 +106,6 @@ class Demo < Sinatra::Base
 
   def avatar_name
     'lion'
-  end
-
-  def run(files, max_seconds = 10)
-    deleted_filenames = []
-    runner.run(image_name, kata_id, avatar_name, deleted_filenames, files, max_seconds)
-  end
-
-  def runner
-    RunnerService.new
-  end
-
-  def read(filename)
-    IO.read("/app/test/start_files/gcc_assert/#{filename}")
-  end
-
-  def timed
-    started = Time.now
-    yield
-    finished = Time.now
-    '%.2f' % (finished - started)
   end
 
   def pre(name, duration, colour='white', sss=nil)
@@ -95,5 +123,3 @@ class Demo < Sinatra::Base
   end
 
 end
-
-
