3fbd712b06c380aaa72aa72ad414ee375b7b4fb9
Jon Jagger <jon@jaggersoft.com>
2017-09-23 12:48:54 +0100

    added fork-bombs tests for Ubuntu to go with the ones for Alpine

--- server/test/src/fork_bomb_test.rb
+++ server/test/src/fork_bomb_test.rb
@@ -18,12 +18,46 @@ class ForkBombTest < TestBase
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # fork-bombs from the source
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test 'CD5',
   %w( [Alpine] fork-bomb in C fails to go off ) do
-    hiker_c = [
-      '#include "hiker.h"',
-      '#include <stdio.h>',
+    hiker_c = '#include "hiker.h"' + "\n" + fork_bomb_definition
+    as_avatar('lion') {
+      sss_run({ avatar_name: 'lion',
+              changed_files: {'hiker.c' => hiker_c }
+      })
+    }
+    assert_status success
+    assert_stderr ''
+    lines = stdout.split("\n")
+    assert lines.count{ |line| line == 'All tests passed' } > 42
+    assert lines.count{ |line| line == 'fork() => 0' } > 42
+    assert lines.count{ |line| line == 'fork() => -1' } > 42
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'CD6',
+  %w( [Ubuntu] fork-bomb in C++ fails to go off ) do
+    hiker_cpp = '#include "hiker.hpp"' + "\n" + fork_bomb_definition
+    as_avatar('lion') {
+      sss_run({ avatar_name: 'lion',
+              changed_files: {'hiker.cpp' => hiker_cpp }
+      })
+    }
+    assert_status 2
+    lines = stdout.split("\n")
+    assert lines.count{ |line| line.include? 'All tests passed' } > 42
+    assert lines.count{ |line| line == 'fork() => 0' } > 42
+    assert lines.count{ |line| line == 'fork() => -1' } > 42
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def fork_bomb_definition
+    [ '#include <stdio.h>',
       '#include <unistd.h>',
       '',
       'int answer(void)',
@@ -39,36 +73,23 @@ class ForkBombTest < TestBase
       '    return 6 * 7;',
       '}'
     ].join("\n")
-    as_avatar('lion') {
-      sss_run({
-          avatar_name:'lion',
-        changed_files:{'hiker.c' => hiker_c }
-      })
-      assert_equal '', stderr
-      lines = stdout.split("\n")
-      assert lines.count{ |line| line == 'All tests passed' } > 42
-      assert lines.count{ |line| line == 'fork() => 0'  } > 42
-      assert lines.count{ |line| line == 'fork() => -1' } > 42
-    }
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # fork-bombs from the shell
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test '4DE',
   %w( [Alpine] fork-bomb in shell fails to go off ) do
-    # The fork-bomb fails in a non-deterministic way.
-    # Occasionally, it throws an ArgumentError exception.
-    # The nocov markers around the block keep coverage at 100%
+    # A shell fork-bomb fails in a non-deterministic way.
+    # Sometimes, it throws an ArgumentError exception.
+    # The nocov markers keep coverage at 100%
     @log = LoggerSpy.new(nil)
-    cyber_dojo_sh = 'bomb() { bomb | bomb & }; bomb'
     as_avatar('lion') {
-      # :nocov:
       begin
-        sss_run({
-            avatar_name:'lion',
-          changed_files:{'cyber-dojo.sh' => cyber_dojo_sh }
-        })
-        assert_equal success, status
+        sss_run_shell_fork_bomb
+      # :nocov:
+        assert_status success
         assert_equal '', stdout
         assert stderr.include? "./cyber-dojo.sh: line 1: can't fork"
       rescue ArgumentError
@@ -78,9 +99,46 @@ class ForkBombTest < TestBase
         assert_equal 'STATUS:2',                      @log.spied[2]
         assert_equal 'STDOUT:',                       @log.spied[3]
         assert_equal "STDERR:sh: can't fork\n",       @log.spied[4]
-      end
       # :nocov:
+      end
     }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test '4DF',
+  %w( [Ubuntu] fork-bomb in shell fails to go off ) do
+    # A shell fork-bomb fails in a non-deterministic way.
+    # Sometimes, it throws an ArgumentError exception.
+    # The nocov markers keep coverage at 100%
+    @log = LoggerSpy.new(nil)
+    as_avatar('lion') {
+      begin
+        sss_run_shell_fork_bomb
+      # :nocov:
+        assert_status success
+        assert_equal '', stdout
+        assert stderr.include? "./cyber-dojo.sh: Cannot fork"
+      rescue ArgumentError
+        rag_filename = '/usr/local/bin/red_amber_green.rb'
+        cmd = "'cat #{rag_filename}'"
+        assert /COMMAND:docker .* sh -c #{cmd}/.match @log.spied[1]
+        assert_equal 'STATUS:2',                      @log.spied[2]
+        assert_equal 'STDOUT:',                       @log.spied[3]
+        assert_equal "STDERR:sh: 1: Cannot fork\n",   @log.spied[4]
+      # :nocov:
+      end
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def sss_run_shell_fork_bomb
+    cyber_dojo_sh = 'bomb() { bomb | bomb & }; bomb'
+    sss_run({
+        avatar_name:'lion',
+      changed_files:{'cyber-dojo.sh' => cyber_dojo_sh }
+    })
   end
 
 end
