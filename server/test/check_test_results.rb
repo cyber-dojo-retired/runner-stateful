36855df858e44445ab0232a3fa62244671607472
Jon Jagger <jon@jaggersoft.com>
2017-09-25 21:27:56 +0200

    refactoring; reduce max hits-per-line

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -101,7 +101,7 @@ table =
     [ 'duration(test)[s]',      test_duration,      '<=', 210 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 300 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 230 ],
     [ 'hits_per_line(test)',    hits_per_line_test, '<=',  15 ],
     [ 'lines(test)/lines(src)', f2(line_ratio),     '>=',   2 ],
   ]
