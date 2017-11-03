239d0202d03ba958aa47283a49cd152c732dad81
Jon Jagger <jon@jaggersoft.com>
2017-11-03 20:46:19 +0000

    delete dead test; pid-1 init is tested in os_helpers.rb:pid_1_process_test()

--- server/test/src/run_alpine_kata_test.rb
+++ server/test/src/run_alpine_kata_test.rb
@@ -27,27 +27,4 @@ class RunAlpineKataTest < TestBase
     assert etc_issue.include?('Alpine'), etc_issue
   end
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  test '214', %w( [Alpine]
-  image has tini installed to do zombie reaping
-  ) do
-    ps = assert_docker_run 'ps'
-    ps_lines = ps.strip.split("\n")
-    # PID   USER     TIME   COMMAND
-    #   1   root     0:00   /sbin/tini -- sh -c sleep 1d
-    #   5   root     0:00   sh -c sleep 1d
-    #   |   |        |      |  |  |
-    #   0   1        2      3  4  5 ...
-    ps_lines.shift
-    procs = Hash[ps_lines.collect { |ps_line|
-      ps_atts = ps_line.split
-      pid = ps_atts[0].to_i
-      cmd = ps_atts[3..-1].join(' ')
-      [pid,cmd]
-    }]
-    refute_nil procs[1], 'no process at pid 1!'
-    assert procs[1].include?('/sbin/tini'), 'no tini at pid 1'
-  end
-
 end
