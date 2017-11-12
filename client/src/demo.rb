a8b5b30b3aa2c11517cecdac661ff6c9ea08dcc3
Jon Jagger <jon@jaggersoft.com>
2017-11-12 17:48:42 +0000

    updated demo

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -5,8 +5,11 @@ class Demo
   def call(_env)
     @html = ''
     in_kata {
-      as_avatar {
-        red; amber; green; time_out
+      as('salmon') {
+        red
+        amber
+        green
+        time_out
       }
     }
     [ 200, { 'Content-Type' => 'text/html' }, [ @html ] ]
@@ -14,50 +17,92 @@ class Demo
 
   private
 
-  def red
-    change = {}
-    duration = timed { @quad = run(change) }
-    @html += pre('run', duration, 'Red', @quad)
-  end
-
-  def amber
-    change = { 'hiker.c' => 'sdsdsdsd' }
-    duration = timed { @quad = run(change) }
-    @html += pre('run', duration, 'Yellow', @quad)
-  end
-
-  def green
-    change = { 'hiker.c' => hiker_c.sub('6 * 9', '6 * 7') }
-    duration = timed { @quad = run(change) }
-    @html += pre('run', duration, 'Lime', @quad)
-  end
-
-  def time_out
-    change = { 'hiker.c' => hiker_c.sub('return', "for(;;);\n return") }
-    duration = timed { @quad = run(change, 3) }
-    @html += pre('run', duration, 'LightGray', @quad)
-  end
-
   def in_kata
-    duration = timed { runner.kata_new(image_name, kata_id) }
+    @image_name = 'cyberdojofoundation/gcc_assert'
+    @kata_id = '729B652756'
+    duration = timed {
+      runner.kata_new(image_name, kata_id)
+    }
     @html += pre('kata_new', duration)
     begin
       yield
     ensure
-      duration = timed { runner.kata_old(image_name, kata_id) }
+      duration = timed {
+        runner.kata_old(image_name, kata_id)
+      }
       @html += pre('kata_old', duration)
     end
   end
 
-  def as_avatar
-    duration = timed { runner.avatar_new(image_name, kata_id, avatar_name, files) }
+  attr_reader :image_name, :kata_id
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  def as(avatar_name)
+    @avatar_name = avatar_name
+    @deleted_files = {}
+    @changed_files = {}
+    @new_files = starting_files
+    duration = timed {
+      runner.avatar_new(image_name, kata_id, avatar_name, new_files)
+      @unchanged_files = new_files
+      @new_files = {}
+    }
     @html += pre('avatar_new', duration)
     begin
       yield
     ensure
-      duration = timed { runner.avatar_old(image_name, kata_id, avatar_name) }
+      duration = timed {
+        runner.avatar_old(image_name, kata_id, avatar_name)
+      }
       @html += pre('avatar_old', duration)
     end
+  end
+
+  attr_reader :avatar_name
+  attr_reader :deleted_files, :unchanged_files, :changed_files, :new_files
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  def red
+    run_cyber_dojo_sh('Red')
+  end
+
+  def amber
+    change('hiker.c', hiker_c.sub('6 * 9', 'syntax-error'))
+    run_cyber_dojo_sh('Yellow')
+  end
+
+  def green
+    change('hiker.c', hiker_c.sub('6 * 9', '6 * 7'))
+    run_cyber_dojo_sh('Green')
+  end
+
+  def time_out
+    change('hiker.c', hiker_c.sub('return', "for(;;);\n return"))
+    run_cyber_dojo_sh('LightGray')
+  end
+
+  def change(filename, content)
+    changed_files[filename] = content
+    unchanged_files.delete(filename)
+  end
+
+  def run_cyber_dojo_sh(colour)
+    quad = nil
+    args = [ image_name, kata_id, avatar_name ]
+    args += [ deleted_files, unchanged_files, changed_files, new_files ]
+    args << 10
+    duration = timed {
+      quad = runner.run_cyber_dojo_sh(*args)
+    }
+    @html += pre('run_cyber_dojo_sh', duration, colour, quad)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  def runner
+    RunnerService.new
   end
 
   def timed
@@ -67,16 +112,7 @@ class Demo
     '%.2f' % (finished - started)
   end
 
-  def runner
-    RunnerService.new
-  end
-
-  def run(files, max_seconds=10)
-    deleted_filenames = []
-    runner.run(image_name, kata_id, avatar_name, deleted_filenames, files, max_seconds)
-  end
-
-  def files
+  def starting_files
     {
       'hiker.c'       => hiker_c,
       'hiker.h'       => read('hiker.h'),
@@ -94,26 +130,15 @@ class Demo
     IO.read("/app/test/start_files/gcc_assert/#{filename}")
   end
 
-  def image_name
-    'cyberdojofoundation/gcc_assert'
-  end
-
-  def kata_id
-    'D4C8A65D61'
-  end
-
-  def avatar_name
-    'lion'
-  end
-
-  def pre(name, duration, colour='white', quad=nil)
-    border = 'border:1px solid black'
-    padding = 'padding:10px'
-    margin = 'margin-left:20px'
-    background = "background:#{colour}"
+  def pre(name, duration, colour = 'white', quad = nil)
+    border = 'border: 1px solid black;'
+    padding = 'padding: 5px;'
+    margin = 'margin-left: 30px; margin-right: 30px;'
+    background = "background: #{colour};"
+    whitespace = "white-space: pre-wrap;"
     html = "<pre>/#{name}(#{duration}s)</pre>"
-    unless quad == nil
-      html += "<pre style='#{margin};#{border};#{padding};#{background}'>" +
+    unless quad.nil?
+      html += "<pre style='#{whitespace}#{margin}#{border}#{padding}#{background}'>" +
               "#{JSON.pretty_unparse(quad)}" +
               '</pre>'
     end
