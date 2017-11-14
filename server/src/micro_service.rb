8353092923379646d1dec28ec7d52c046cfd0808
Jon Jagger <jon@jaggersoft.com>
2017-11-14 12:25:47 +0000

    refactoring

--- server/src/micro_service.rb
+++ server/src/micro_service.rb
@@ -6,41 +6,42 @@ class MicroService
 
   def call(env)
     request = Rack::Request.new(env)
-    @args = JSON.parse(request.body.read)
-    case request.path_info
-      when /image_pulled?/
-        body = invoke('image_pulled?')
-      when /image_pull/
-        body = invoke('image_pull')
-      when /kata_new/
-        body = invoke('kata_new')
-      when /kata_old/
-        body = invoke('kata_old')
-      when /avatar_new/
-        body = invoke('avatar_new', avatar_name, starting_files)
-      when /avatar_old/
-        body = invoke('avatar_old', avatar_name)
-      when /run_cyber_dojo_sh/
-        body = invoke('run_cyber_dojo_sh',
-          avatar_name,
-          new_files, deleted_files, unchanged_files, changed_files,
-          max_seconds)
-      when /run/
-        args  = [ avatar_name ]
-        args += [ deleted_filenames, changed_files ]
-        args += [ max_seconds ]
-        body = invoke('run', *args)
+    @json = JSON.parse(request.body.read)
+    @name = request.path_info[1..-1] # lose leading /
+    @args = case @name
+      when /^image_pulled$/
+        @name += '?'
+        []
+      when /^image_pull$/
+        []
+      when /^kata_new$/
+        []
+      when /^kata_old$/
+        []
+      when /^avatar_new$/
+        [avatar_name, starting_files]
+      when /^avatar_old$/
+        [avatar_name]
+      when /^run_cyber_dojo_sh$/
+        [avatar_name,
+         new_files, deleted_files, unchanged_files, changed_files,
+         max_seconds]
+      when /^run$/
+        [avatar_name, deleted_filenames, changed_files, max_seconds]
+      else
+        @name = nil
+        []
     end
-    [ 200, { 'Content-Type' => 'application/json' }, [ body.to_json ] ]
+    [ 200, { 'Content-Type' => 'application/json' }, [ invoke.to_json ] ]
   end
 
   private
 
-  def invoke(name, *args)
+  def invoke
     runner = Runner.new(self, image_name, kata_id)
-    { name => runner.send(name, *args) }
+    { @name => runner.send(@name, *@args) }
   rescue Exception => e
-    log << "EXCEPTION: #{e.class.name}.#{name} #{e.message}"
+    log << "EXCEPTION: #{e.class.name}.#{@name} #{e.message}"
     { 'exception' => e.message }
   end
 
@@ -50,18 +51,13 @@ class MicroService
 
   def self.request_args(*names)
     names.each { |name|
-      define_method name, &lambda { @args[name.to_s] }
+      define_method name, &lambda { @json[name.to_s] }
     }
   end
 
-  request_args :image_name
-  request_args :kata_id
-  request_args :avatar_name
+  request_args :image_name, :kata_id, :avatar_name
   request_args :starting_files
-  request_args :deleted_files
-  request_args :unchanged_files
-  request_args :changed_files
-  request_args :new_files
+  request_args :new_files, :deleted_files, :unchanged_files, :changed_files
   request_args :max_seconds
 
   request_args :deleted_filenames
