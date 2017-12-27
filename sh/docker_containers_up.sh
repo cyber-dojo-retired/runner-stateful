116443e081b01a2e8c6aec690ac0823b9d36228c
Jon Jagger <jon@jaggersoft.com>
2017-12-27 08:29:24 +0000

    refactoring; improved container names

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -24,6 +24,5 @@ docker-compose \
   up -d \
   --force-recreate
 
-
-wait_till_up 'runner_stateful_server'
-wait_till_up 'runner_stateful_client'
+wait_till_up 'test_runner_stateful_server'
+wait_till_up 'test_runner_stateful_client'
